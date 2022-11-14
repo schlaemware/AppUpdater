58b26e5cf730c0811047ed22d5b2e41832a020cf
Michael Schläpfer <schlaem91@gmail.com>
2022-11-14 16:03:07 +0100

    Download installer implemented

--- Solution/DevConsole/Program.cs
+++ Solution/DevConsole/Program.cs
@@ -1,4 +1,5 @@
-﻿using SW.AppUpdater.Abstractions.Contracts;
+﻿using System.Runtime.CompilerServices;
+using SW.AppUpdater.Abstractions.Contracts;
 using SW.AppUpdater.GitHub;
 
 internal class Program {
@@ -10,9 +11,19 @@ internal class Program {
 
     IAppUpdater updater = new GitHubAppUpdater();
     updater.ReleaseFound += new EventHandler<SW.AppUpdater.Abstractions.Models.Release>((sender, release) => {
+      string filePath = release.HasInstaller ? updater.LoadInstaller(release).Result : string.Empty;
+
       Console.WriteLine($"\tRelease {release.Version}");
       Console.WriteLine($"\t\tIs Prerelease: {release.IsPreRelease}");
       Console.WriteLine($"\t\tHas installer: {release.HasInstaller}");
+      Console.WriteLine();
+
+      if (release.HasInstaller) {
+        Console.WriteLine($"\t\tLoad installer: {(File.Exists(filePath) ? "Success" : "Failed")}");
+      } else {
+        Console.WriteLine($"\t\tLoad installer: No installer...");
+      }
+
       Console.WriteLine();
     });
     updater.CheckForUpdates(organization, application, null, true);
